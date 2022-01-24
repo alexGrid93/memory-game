32ffa99e44c3ef490b713fa23ce112a237b50e98
Alexander Gridyaev <alexandergr1993o@gmail.com>
2022-01-24 21:11:44 +0300

    create class game-scene

---
+++ assets/scripts/GameScene.js
@@ -0,0 +1,59 @@
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("Game");
+  }
+
+  preload() {
+    this.load.image("bg", "assets/sprites/background.png");
+    this.load.image("card", "assets/sprites/card.png");
+  }
+
+  create() {
+    this.add.sprite(0, 0, "bg").setOrigin(0, 0);
+
+    const positionsForCards = this.generateCardPosition();
+
+    positionsForCards.map((position) => {
+      this.add.sprite(position.x, position.y, "card").setOrigin(0, 0);
+    });
+  }
+
+  generateCardPosition() {
+    const card = {
+      width: 196,
+      height: 306,
+      marginRight: 10,
+      marginBottom: 10,
+    };
+
+    // размеры поля для расположения карточек
+    const cardsArea = {
+      width: card.width * config.cols + (config.cols - 1) * card.marginRight,
+      height: card.height * config.rows + (config.rows - 1) * card.marginBottom,
+    };
+
+    // размеры отступов для карточки
+    const offset = {
+      x: (config.width - cardsArea.width) / 2,
+      y: (config.height - cardsArea.height) / 2,
+    };
+
+    // генерируем координаты для каждой карточки с учётом отступа от краёв и отступа между карточками
+    const positions = [];
+
+    for (let row = 0; row < config.rows; row++) {
+      for (let col = 0; col < config.cols; col++) {
+        positions.push({
+          x:
+            offset.x +
+            col * (col === 0 ? card.width : card.width + card.marginRight),
+          y:
+            offset.y +
+            row * (row === 0 ? card.height : card.height + card.marginBottom),
+        });
+      }
+    }
+
+    return positions;
+  }
+}
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,68 +1,10 @@
-let scene = new Phaser.Scene("Game");
-
-scene.preload = function () {
-  // 1. загрузить бэкгранд
-  this.load.image("bg", "assets/sprites/background.png");
-  this.load.image("card", "assets/sprites/card.png");
-};
-
-scene.create = function () {
-  this.add.sprite(0, 0, "bg").setOrigin(0, 0);
-
-  const positionsForCards = scene.generateCardPosition();
-
-  positionsForCards.map((position) => {
-    this.add.sprite(position.x, position.y, "card").setOrigin(0, 0);
-  });
-};
-
-scene.generateCardPosition = () => {
-  // параметры картоки
-  const card = {
-    width: 196,
-    height: 306,
-    marginRight: 10,
-    marginBottom: 10,
-  };
-
-  // размеры поля для расположения карточек
-  const cardsArea = {
-    width: card.width * config.cols + (config.cols - 1) * card.marginRight,
-    height: card.height * config.rows + (config.rows - 1) * card.marginBottom,
-  };
-
-  // размеры отступов для карточки
-  const offset = {
-    x: (config.width - cardsArea.width) / 2,
-    y: (config.height - cardsArea.height) / 2,
-  };
-
-  // генерируем координаты для каждой карточки с учётом отступа от краёв и отступа между карточками
-  const positions = [];
-
-  for (let row = 0; row < config.rows; row++) {
-    for (let col = 0; col < config.cols; col++) {
-      positions.push({
-        x:
-          offset.x +
-          col * (col === 0 ? card.width : card.width + card.marginRight),
-        y:
-          offset.y +
-          row * (row === 0 ? card.height : card.height + card.marginBottom),
-      });
-    }
-  }
-
-  return positions;
-};
-
 let config = {
   type: Phaser.AUTO, // webgl or canvas
   width: 1280,
   height: 720,
   rows: 2,
   cols: 5,
-  scene: scene,
+  scene: new GameScene(),
 };
 
-let game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
